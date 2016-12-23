ebc34f842a579d67c0836f942898771a03b96b7b
Cédric Chantepie <cchantep@users.noreply.github.com>
2016-12-23 19:28:32 +0100

    Release 0.12.1 (#49)
    * Update to Scala 2.12 (#47)
    
    * Update to Scala 2.12
    
    * Update plugins.sbt
    
    * Release 0.12.1

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,6 +5,10 @@ addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.5.0")
 
 addSbtPlugin("cchantep" % "sbt-hl-compiler" % "0.2")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
+
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.8.2")
+
+addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.8")
 
 addSbtPlugin("de.johoop" % "findbugs4sbt" % "1.4.0")
--- src/test/scala/Common.scala
+++ src/test/scala/Common.scala
@@ -3,15 +3,17 @@ import scala.concurrent.duration._
 import reactivemongo.api.MongoDriver
 
 object Common {
-  implicit val ec = ExecutionContext.Implicits.global
-
   val timeout = 5 seconds
   val timeoutMillis = timeout.toMillis.toInt
 
   lazy val driver = new MongoDriver()
   lazy val connection = driver.connection(List("localhost:27017"))
-  lazy val db = Await.result(connection.database("specs2-test-reactivemongo").
-    flatMap { _db => _db.drop.map(_ => _db) }, timeout)
+  lazy val db = {
+    implicit def ec = ExecutionContext.Implicits.global
+
+    Await.result(connection.database("specs2-test-reactivemongo").
+      flatMap { _db => _db.drop.map(_ => _db) }, timeout)
+  }
 
   def close(): Unit = try {
     driver.close()
